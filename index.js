e3eed397bbf3f49de4e4c711f95e127260c4d66c
Matthew Trenk <matt.trenk@gmail.com>
2018-01-18 18:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,32 @@ function preserveDecimal(n){
   return n
 }
 
-function add
+function add(a, b){
+  sum = a + b
+  return sum
+}
+
+function subtract(a, b){
+  x = a - b
+  return x
+}
+
+function multiply (a, b){
+  product = a*b
+  return product
+}
+
+function divide(a, b){
+  v = a/b
+  return v
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(n) {
+  n -= 1
+  return n
+}
